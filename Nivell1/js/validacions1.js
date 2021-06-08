d78394bd4acf730f945eb3f511bb103237107ec8
Barceboba <miquel@barceboba.com>
2021-06-08 18:34:00 +0200

    Reparacions de a trobada

--- Nivell1/js/validacions1.js
+++ Nivell1/js/validacions1.js
@@ -1,13 +1,14 @@
 //Inputs html
 const form = document.getElementById("myFormId");
 
+//Funcio validar accés
 function validacioAcces() {
 
     //Variable acumulació errors a zero
     let aplegaErrors = 0;
 
     
-    let entraMail = document.getElementById('entrMail');
+    let entraMail = document.getElementById('entraMail');
     let entraPass = document.getElementById('entraPass');
 
     //Condicionals errors
@@ -33,9 +34,17 @@ function validacioAcces() {
     }
 }
 
+
+//Funcio registre usuari
+function entradaRegistre() {
+
+}
+
+
+
 //Efecte blur
-/*form.addEventListener('blur', (event) => {
+form.addEventListener('blur', (event) => {
 	console.log(event);
 	if(event.target.value!='') event.target.classList.remove('is-invalid');
     //registerValidate();
-}, true);*/
+}, true);
