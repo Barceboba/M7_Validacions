617498018a99d80327c2a054de9f11c18bb3c8b3
Barceboba <miquel@barceboba.com>
2021-06-09 10:19:45 +0200

    Ja pita, arreclar sintaxis

--- Nivell1/js/validacions1.js
+++ Nivell1/js/validacions1.js
@@ -1,24 +1,26 @@
 //Inputs html
 const form = document.getElementById("myFormId");
 
+ //Variable acumulació errors a zero
+ let aplegaErrors = 0;
+ 
 //Funcio validar accés
 function validacioAcces() {
 
-    //Variable acumulació errors a zero
-    let aplegaErrors = 0;
+   
 
-    
-    let entraMail = document.getElementById('entraMail');
-    let entraPass = document.getElementById('entraPass');
+    //declaracio variables log
+    // let entraMail = document.getElementById('entraMail');
+    // let entraPass = document.getElementById('entraPass');
 
     //Condicionals errors
-    
+
 
     if (entraMail.value == "") {
         entraMail.classList.add('is-invalid');
         document.getElementById('errorMail').textContent = 'El camp és obligat';
         aplegaErrors++;
-    
+
     }
 
     if (entraPass.value == "") {
@@ -38,13 +40,58 @@ function validacioAcces() {
 //Funcio registre usuari
 function entradaRegistre() {
 
+
+
+    //declaració variables regitres
+
+
+    //Condicions camps vuits
+    if (entraNom.value == '') {
+        entraNom.classList.add('is-invalid');
+        document.getElementById('errorNom').textContent = 'El camp és obligatori';
+        aplegaErrors ++;
+    }
+
+    if (introMail.value == '') {
+        introMail.classList.add('is-invalid');
+        document.getElementById('errorIntroMail').textContent = 'El camp és obligatori';
+        aplegaErrors ++;
+    }
+
+    if (inputPassword.value == '') {
+        inputPassword.classList.add('is-invalid');
+        document.getElementById('errorPassword').textContent = 'El camp és obligatori';
+        aplegaErrors ++;
+    }
+
+    if (inputPassword2.value == '') {
+        inputPassword2.classList.add('is-invalid');
+        document.getElementById('errorPassword2').textContent = 'El camp és obligatori';
+        aplegaErrors ++;
+    }
+
+    if (inputProvince.value == '') {
+        inputProvince.classList.add('is-invalid');
+        document.getElementById('errorProvince').textContent = 'Triar província';
+        aplegaErrors ++;
+
+    }
+
+    if (aplegaErrors > 0) {
+        return false;
+    } else {
+        return true;
+    }
+
+
+
 }
 
 
 
 //Efecte blur
 form.addEventListener('blur', (event) => {
-	console.log(event);
-	if(event.target.value!='') event.target.classList.remove('is-invalid');
+    console.log(event);
+    if (event.target.value != '') event.target.classList.remove('is-invalid');
     //registerValidate();
 }, true);
